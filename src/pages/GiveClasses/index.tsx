d25b692f5c824983eeca55ecab491eaebc5cb3e8
jairosl <jairo.lima@aluno.uepb.edu.br>
2020-08-06 12:55:16 -0300

    Create GiveClasses Page

--- src/pages/GiveClasses/index.tsx
+++ src/pages/GiveClasses/index.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
-import { View } from 'react-native';
+import { ImageBackground, Text, View } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import styles from './styles';
 
+import giveClassesBgImage from '../../assets/images/give-classes-background.png'
+import { useNavigation } from '@react-navigation/native';
+
 const GiveClasses: React.FC = () => {
-  return <View style={styles.container}/>;
+
+  const { goBack } = useNavigation();
+
+  function handleNavigateBack() {
+    goBack();
+  }
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground 
+        source={giveClassesBgImage} 
+        style={styles.content}
+        resizeMode='contain'
+      >
+        <Text style={styles.title}>Quer ser um Proffy?</Text>
+        <Text style={styles.description}>
+          Para começar, 
+          você precisa se cadastra  como professor na nosssa plataforma web.
+        </Text>
+      </ImageBackground>
+
+      <RectButton 
+        style={styles.okButton}
+        onPress={handleNavigateBack}
+      >
+        <Text style={styles.okButtonText}>Tudo Bem</Text>
+      </RectButton>
+    </View>
+  );
 }
 
 export default GiveClasses;
